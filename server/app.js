7cafd1a4fc6743980f9e3a0b432673014faafb80
서지상 <inaki96@naver.com>
2021-04-13 01:45:24 +0900

    db con nect

--- server/app.js
+++ server/app.js
@@ -1,19 +1,25 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
+import logger from 'morgan';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
 
 import { breakSecurityPolicy, globalMiddleWare } from './middleware.js';
 import routes from './routes';
 import homeRouter from './router/homeRouter.js';
 import transActionRouter from './router/transActionRouter';
 import mainRouter from './router/mainRouter';
+import './passport';
 
 const app = express();
 
-app.use(helmet());
-app.use(breakSecurityPolicy);
+const CookieStore = MongoStore(session);
+
+app.use(helmet({ contentSecurityPolicy: false }));
 
 app.set('view engine', 'pug');
 app.set('views', './view');
@@ -22,6 +28,18 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
+
+app.use(
+	session({
+		secret: ``,
+		resave: true,
+		saveUninitialized: false,
+		store: new CookieStore({ mongooseConnection: mongoose.connection })
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('*', globalMiddleWare);
 
---
+++ server/passport.js
@@ -0,0 +1,9 @@
+import passport from 'passport';
+
+/*
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+*/
